54fc71ccb3e92973ce2da12e8386be09c45ad0b6
Anton Filippov <derevenchshina@yandex.ru>
2025-06-03 18:09:48 +0500

    [otus-project] Добавил javadoc

--- src/main/scala/ru/filippov/otus/roombooking/config/AppConfig.scala
+++ src/main/scala/ru/filippov/otus/roombooking/config/AppConfig.scala
@@ -4,6 +4,12 @@ import pureconfig.generic.auto._
 
 case class AppConfig(port: Int, host: String)
 
+/**
+ * Метод load используется для загрузки конфигурации приложения.
+ * Он использует ConfigSource для загрузки конфигурации из источника по умолчанию.
+ *
+ * @return AppConfig - объект конфигурации приложения.
+ */
 object AppConfig {
   def load(): AppConfig = {
     ConfigSource.default.at("app.server").loadOrThrow[AppConfig]
